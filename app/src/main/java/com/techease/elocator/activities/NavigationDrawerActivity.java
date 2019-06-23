3bb85bfdb9d1dc5ea023d3f32d964ecb9743c0ff
eapple <softwrengr@gmail.com>
2019-06-23 21:15:07 +0500

    adding store and showing store work done

--- app/src/main/java/com/techease/elocator/activities/NavigationDrawerActivity.java
+++ app/src/main/java/com/techease/elocator/activities/NavigationDrawerActivity.java
@@ -31,14 +31,6 @@ public class NavigationDrawerActivity extends AppCompatActivity
 
         GeneralUtils.connectFragmentWithDrawer(this, new StoreFragment());
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                GeneralUtils.connectFragmentWithDrawer(NavigationDrawerActivity.this, new AddStoreFragment());
-            }
-        });
-
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
